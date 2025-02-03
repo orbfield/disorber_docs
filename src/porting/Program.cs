a1f63635491c673cc9ad797b67609e6c72175c99
Olly Field <ollyfield@hotmail.com>
2025-02-03 06:54:16 +0000

    removed code remenant, added .VSCodeCounter

--- src/porting/Program.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Disorber.Client.Game.Services;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-// Register SidebarService as a scoped service
-builder.Services.AddScoped<SidebarService>();
-
-await builder.Build().RunAsync();
